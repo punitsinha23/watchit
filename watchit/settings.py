c8304018afe3fa3d37e7a840065c0b560750ab4f
punit sinha <162338818+punitsinha23@users.noreply.github.com>
2024-12-22 16:39:56 +0000

    edited

--- watchit/settings.py
+++ watchit/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'watchit_app',
-    'whitenoise.runserver_nostatic'
+    'watchit_app'
 ]
 
 MIDDLEWARE = [
