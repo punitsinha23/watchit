eff0db1fbb697c0135c1cc16ad2f232628d344fd
punit sinha <162338818+punitsinha23@users.noreply.github.com>
2025-12-22 21:52:32 +0530

    feat: Add initial Django settings, including allauth, environment-based database, static files, and email configuration.

--- watchit/settings.py
+++ watchit/settings.py
@@ -154,7 +154,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # CACHE SETTINGS
 CACHES = {
